0d940e84cac01feb4472e717169906d192d76cc6
Denis <jsherr.6@gmail.com>
2022-09-23 16:52:41 -0500

    evn

--- database/config.js
+++ database/config.js
@@ -6,7 +6,7 @@ const dbConnection = async() =>{
      mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}
     //  mongodb://mongo:qC2NmB541kFTZD2bYs98@containers-us-west-52.railway.app:7318
     // mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@team.rrgol.mongodb.net/mern_calendar
-    await mongoose.connect(process.env.CNN);
+    await mongoose.connect('mongodb://mongo:qC2NmB541kFTZD2bYs98@containers-us-west-52.railway.app:7318');
     console.log('BD online');
   } catch (error) {
     throw new Error('error a la hora de inciar la BD')
