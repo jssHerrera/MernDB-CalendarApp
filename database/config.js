f0f859156f8dffd9da7b0a7ec942ef7ca15df0b1
Denis <jsherr.6@gmail.com>
2022-09-23 16:26:06 -0500

    evn

--- database/config.js
+++ database/config.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const dbConnection = async() =>{
   try {
      // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
-    await mongoose.connect(process.env.DB_CNN);
+     mongodb://${{ MONGOUSER }}:${{ MONGOPASSWORD }}@${{ MONGOHOST }}:${{ MONGOPORT }}
+    await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@team.rrgol.mongodb.net/mern_calendar`);
     console.log('BD online');
   } catch (error) {
     throw new Error('error a la hora de inciar la BD')
