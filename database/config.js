cd5a6cca628327dab14e98b62568f2f10c1d455b
Denis <jsherr.6@gmail.com>
2022-09-24 17:31:00 -0500

    env

--- database/config.js
+++ database/config.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() =>{
   try {
-     // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
-    await mongoose.connect(process.env.DB_CNN);
+    // use `await mongoose.connect('mongodb://user:password@localhost:27017/');` if your database has auth enabled
+    // process.env.DB_CNN
+    "mongodb://merm_user:gH49Lpkpot6Y2D50@localhost:27017/mern_calendar"
+    await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
     console.log('BD online');
   } catch (error) {
     throw new Error('error a la hora de inciar la BD')
@@ -13,3 +15,5 @@ const dbConnection = async() =>{
 module.exports = {
   dbConnection
 }
+
+
